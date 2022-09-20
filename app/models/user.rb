e59d15d62ac54c3806beb52945839c31a3ea33b1
Red <crabmarket56@gmail.com>
2022-09-20 15:53:27 +0900

    Generate and migrate User

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :password, presence: true
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
