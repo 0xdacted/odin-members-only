dd2de621bb555a8f188a714c59c662a39bbf0d4e
Red <crabmarket56@gmail.com>
2022-09-21 20:21:26 +0900

    Add username migration and devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
-  
+  validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
   
