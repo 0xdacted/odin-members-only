e4941708154b711697674a4bda874e53df03b547
Red <crabmarket56@gmail.com>
2022-09-21 09:55:39 +0900

    Draft posts controller logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,22 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  
+  def index
+    @posts = Post.all
+  end
+  
   def new
     @post = Post.new
   end
 
   def create
     @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] "Your post was successful"
+      redirect_to index
+    else
+      flash[:error] "Your post was unsuccessful"
+      render 'new'
   end
 
   private
