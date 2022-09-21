70de572263f63b637410687ba3e97e1c68f0ccb7
Red <crabmarket56@gmail.com>
2022-09-21 11:15:40 +0900

    Add index view

--- db/migrate/20220921001207_create_posts.rb
+++ db/migrate/20220921001207_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.string :body
-      t.belongs_to :user, foreign_key: true
+      t.belongs_to :user, foreign_key: "user_id"
       t.timestamps
     end
   end
