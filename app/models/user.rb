5fc3c25a70f99503c157a78e4a3d3c473e224608
Red <crabmarket56@gmail.com>
2022-09-21 09:29:10 +0900

    Add model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts
+  
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true
   validates :password, presence: true
